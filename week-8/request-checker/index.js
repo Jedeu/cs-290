ed8f9c11e7ab5de9db91ec4d56a69980b0805728
Jedeu <jed.piezas@gmail.com>
2019-03-03 20:59:30 -0800

    Move request-checker to week-8 and gitignore the dbconnection stuff

---
+++ week-8/request-checker/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 1990;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('index', {title: "Pug example", welcomeMessage: "Welcome to the GET and POST checker!"})
+});
+
+app.get('/api/v1/_test', (req, res) => {
+    res.render('test', {title: "GET Request", message: "GET request received!", params: req.query});
+});
+
+app.post('/api/v1/_test', (req, res) => {
+    res.render('test', {title: "POST Request", message: "POST request received!", params: req.body})
+})
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
